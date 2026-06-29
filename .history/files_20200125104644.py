f5f934cd80f4d401f0e3a47e5e92b2119324e4f6
artchiee <53340008+artchiee@users.noreply.github.com>
2026-06-29 23:31:38 +0100

    repo updated

--- .history/files_20200125104644.py
+++
@@ -1,7 +0,0 @@
-
-class Files:
-    def __init__(self, name, file_type, size, url): 
-        self.name = name
-        self.file_type = file_type  # Means is it a movie or a  Tv show 
-        self.size = size 
-        self.url  = url 
